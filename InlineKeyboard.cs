1c0aa2d52d7b51b8ec7e461fa0bc2d11ddd555f7
Lena Runees <lenaruneescape@gmail.com>
2025-04-21 13:50:51 +0400

    Update InlineKeyboard.cs

--- InlineKeyboard.cs
+++ InlineKeyboard.cs
@@ -5,7 +5,8 @@ namespace MyBot
 public static class InlineKeyboard
 {
     public static InlineKeyboardMarkup BuildAdminButton(long userID)
-    {   
+    {
+                        //Inline keyboard   
         var inlineKeyboard = new InlineKeyboardMarkup(new[]
         {
             new[]
@@ -17,7 +18,19 @@ public static class InlineKeyboard
 
     }
 
-    public static InlineKeyboardMarkup BuildUnbanButton(long userID)
+    public static InlineKeyboardMarkup BuildLanguageButton(long userID)
+    {
+        var inlineKeyboard3 = new InlineKeyboardMarkup(new[]
+        {
+            new[]
+            {
+                InlineKeyboardButton.WithCallbackData("🇺🇸 English", $"eng-{userID}"),
+                InlineKeyboardButton.WithCallbackData("🏳️‍⚧️ Russian", $"rus-{userID}")
+            }});
+        return inlineKeyboard3;
+    }
+
+        public static InlineKeyboardMarkup BuildUnbanButton(long userID)
     {
         var inlineKeyboard2 = new InlineKeyboardMarkup(new[]
         {
